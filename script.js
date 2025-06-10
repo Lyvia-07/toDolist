53aa8b2183b33f5c6e348f6bf6f287807f951af2
Lyvia-07 <lyvia.m.lima@aluno.senai.br>
2025-06-10 17:28:45 -0300

    Lista de tarefas fubcional

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // definir variaves para o uso do codigo
-const btnAdd = document.getElementById('btn-add');
+const btnAdd = document.getElementById('btnAdd');
 const tarefa = document.getElementById('tarefa');
 const taskList = document.getElementById('taskList');
 const titulo = document.getElementById('titulo');
@@ -11,5 +11,32 @@ titulo.innerHTML = `Lista de tarefa: ${nome}`;
 btnAdd.addEventListener("click", criaTarefa);
 
 function criaTarefa(){
+   const listItem = document.createElement('li');
+    listItem.textContent = tarefa.value;
+    taskList.appendChild(listItem);
+
+    const removeButton = document.createElement('button');
+    removeButton.id ="remove";
+    removeButton.textContent = "x"
+    
+
+    removeButton.addEventListener("click", function(){
+        taskList.removeChild(listItem);
+    })
+
+    const concluirButton = document.createElement('button');
+    concluirButton.id ="conclui";
+    concluirButton.textContent = "✓"
+    
+    let buttonsItem= document.createElement('div');
+    buttonsItem.classList.toggle('buttonsItem')
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirButton);
+    buttonsItem.appendChild(removeButton);
+
+
+    concluirButton.addEventListener("click", function(){
+        listItem.classList.toggle('completed');
+    })
 
 }
